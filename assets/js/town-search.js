453d0151c50ce80f4538c73a0e17be0ec66d3d7d
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2019-10-29 22:11:12 -0400

    Send search strings to Google Analytics
    Debounce with an interval of 1s.

--- assets/js/town-search.js
+++ assets/js/town-search.js
@@ -13,6 +13,31 @@
 })(jQuery);
 
 $(document).ready(function() {
+  function sendSearchEvent(searchStr) {
+    window.dataLayer.push(
+      {'event': 'search-str-update', 'searchStr': searchStr }
+    );
+  }
+
+  // Only call sendSearchEvent once the user has stopped typing for
+  // at least SEARCH_EVENT_DEBOUNCE_INTERVAL milliseconds, and then
+  // only if the value has changed since the last time sendSearchEvent
+  // was called.
+  var SEARCH_EVENT_DEBOUNCE_INTERVAL = 1000;
+  var timeoutId;
+  var lastEvent = { value: null }
+  function debounceSendSearchEvent(searchStr) {
+    if (timeoutId) {
+      window.clearTimeout(timeoutId);
+    }
+    if (searchStr && searchStr !== lastEvent.value) {
+      timeoutId = window.setTimeout(function() {
+        lastEvent.value = searchStr;
+        sendSearchEvent(searchStr)
+      }, SEARCH_EVENT_DEBOUNCE_INTERVAL);
+    }
+  }
+
   $('input#town-search').change(function() {
     var search_str = $(this).val().toLowerCase();
 
@@ -22,6 +47,7 @@ $(document).ready(function() {
     } else {
       $('tr').show()
     }
+    debounceSendSearchEvent(search_str);
   }).keyup(function() {
     $(this).change();
   })
