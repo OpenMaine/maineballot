d2f56b874d7594d8ad7b629a5e546144c7ca1dd7
rentry <brent.ryan.johnson@gmail.com>
2019-12-06 15:54:03 -0800

    Adds _site for Netlify deploy

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,96 @@
+var store = [{
+        "title": "Question 1: Recreational Marijuana",
+        "excerpt":"Results This measure passed. The gist If passed, this referendum would legalized marijuana for recreational use. Ballot question “Do you want to allow the possession and use of marijuana under state law by persons who are at least 21 years of age, and allow the cultivation, manufacture, distribution, testing, and...","categories": ["November 2016 Election"],
+        "tags": ["Marijuana","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/november%202016%20election/Q1_Marijuana/",
+        "teaser":"https://maineballot.org/assets/img/2016_11/Question1_Marijuana.jpg"},{
+        "title": "Question 2: Education Funding",
+        "excerpt":"Results This measure passed. The gist If passed, this referendum would add a 3% tax on income above $200,000 to create a fund to increase teacher salaries. Ballot question “Do you want to add a 3% tax on individual Maine taxable income above $200,000 to create a state fund that...","categories": ["November 2016 Election"],
+        "tags": ["Education","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/november%202016%20election/Q2_Education/",
+        "teaser":"https://maineballot.org/assets/img/2016_11/Question2_Education.jpg"},{
+        "title": "Question 3: Firearm Sales",
+        "excerpt":"Results This measure did not pass. The gist Currently, guns that are sold by anyone who is not a licensed dealer (think Craigslist ads and other private sales) don’t require a background check. If passed, this referendum would require all unlicensed gun sellers (with a few exceptions) to get a...","categories": ["November 2016 Election"],
+        "tags": ["Guns","Citizen Initiative","Did Not Pass"],
+        "url": "https://maineballot.org/november%202016%20election/Q3_Firearms/",
+        "teaser":"https://maineballot.org/assets/img/2016_11/Question3_Firearms.jpg"},{
+        "title": "Question 4: Minimum Wage",
+        "excerpt":"Results This measure passed. The gist This referendum would raise the minimum wage by $1 a year until it gets to $12 in 2020. It will also slowly eliminate the tip credit, which tip earning employers pay to make up the difference between wages plus tips and the minimum wage...","categories": ["November 2016 Election"],
+        "tags": ["Wages","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/november%202016%20election/Q4_MinimumWage/",
+        "teaser":"https://maineballot.org/assets/img/2016_11/Question4_MinimumWage.jpg"},{
+        "title": "Question 5: Ranked-Choice Voting",
+        "excerpt":"Results This measure passed. The gist Ever wanted a third party or independent candidate to win an election? If this referendum passes, it would be more possible. Ballot question “Do you want to allow voters to rank their choices of candidates in elections for U.S. Senate, Congress, Governor, State Senate,...","categories": ["November 2016 Election"],
+        "tags": ["Ranked-Choice","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/november%202016%20election/Q5_RankedChoiceVoting/",
+        "teaser":"https://maineballot.org/assets/img/2016_11/Question5_RankedChoice.jpg"},{
+        "title": "Question 6: Transportation",
+        "excerpt":"Results This measure passed. The gist This referendum would raise $100 million to pay for transportation upgrades and maintenance. Ballot question “Do you favor a $100,000,000 bond issue for construction, reconstruction and rehabilitation of highways and bridges and for facilities, equipment and property acquisition related to ports, harbors, marine transportation,...","categories": ["November 2016 Election"],
+        "tags": ["Transportation","Bond","Passed"],
+        "url": "https://maineballot.org/november%202016%20election/Q6_Transportation/",
+        "teaser":"https://maineballot.org/assets/img/2016_11/Question6_Transportation.jpg"},{
+        "title": "Question 1: Technology and Business Loans Bond",
+        "excerpt":"Results This measure passed. The gist If passed, this referendum would issue $45 million in bonds to fund the Maine Technology Institute (MTI) and $5 million in bonds to fund the Maine Venture Fund (MVF). Ballot question “Do you favor a $50,000,000 bond issue to provide $45,000,000 in funds for...","categories": ["June 2017 Election"],
+        "tags": ["Business","Bond","Passed"],
+        "url": "https://maineballot.org/june%202017%20election/Q1_Business/",
+        "teaser":"https://maineballot.org/assets/img/2017_06/Question1_Business.jpg"},{
+        "title": "Question 1: York County Casino",
+        "excerpt":"Results This measure did not pass. The gist This referendum would change the rules to allow Capital Seven LLC, the owner of Bangor Raceway, to apply for a casino license in York County. Voters in the municipality where it is located would have to approve the casino’s operation if it...","categories": ["November 2017 Election"],
+        "tags": ["Casino","Citizen Initiative","Did Not Pass"],
+        "url": "https://maineballot.org/november%202017%20election/Q1_Casino/",
+        "teaser":"https://maineballot.org/assets/img/2017_11/Question1_Casino.jpg"},{
+        "title": "Question 2: Medicaid Expansion",
+        "excerpt":"Results This measure passed. The gist This referendum would expand Medicaid coverage in accordance with the Affordable Care Act. Ballot question “Do you want Maine to expand Medicaid to provide healthcare coverage for qualified adults under age 65 with incomes at or below 138% of the federal poverty level, which...","categories": ["November 2017 Election"],
+        "tags": ["Healthcare","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/november%202017%20election/Q2_Medicaid/",
+        "teaser":"https://maineballot.org/assets/img/2017_11/Question2_Medicaid.jpg"},{
+        "title": "Question 3: Transportation",
+        "excerpt":"Results This measure passed. The gist This referendum would allow the state to raise $105 million to pay for transportation upgrades and maintenance. Ballot question “Do you favor a $105,000,000 bond issue for construction, reconstruction and rehabilitation of highways and bridges and for facilities or equipment related to ports, harbors,...","categories": ["November 2017 Election"],
+        "tags": ["Transportation","Bond","Passed"],
+        "url": "https://maineballot.org/november%202017%20election/Q3_Transportation/",
+        "teaser":"https://maineballot.org/assets/img/2017_11/Question3_Transportation.jpg"},{
+        "title": "Question 4: Public Pensions",
+        "excerpt":"Results This measure passed. The gist This referendum would spread out losses for public pensions over 20 years instead of the current practice of 10 years. Ballot question “Do you favor amending the Constitution of Maine to reduce volatility in state pension funding requirements caused by the financial markets by...","categories": ["November 2017 Election"],
+        "tags": ["Pensions","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/november%202017%20election/Q4_Pension/",
+        "teaser":"https://maineballot.org/assets/img/2017_11/Question4_Pension.jpg"},{
+        "title": "Question 1: Ranked-Choice Voting",
+        "excerpt":"Results This measure passed. The gist Ever wanted a third party or independent candidate to win an election? If this referendum passes, it would be even more possible than it was when voters voted for it in the November 2016 referendum election. Ballot question “Do you want to reject the...","categories": ["June 2018 Election"],
+        "tags": ["Ranked-Choice","Citizen Initiative","Passed"],
+        "url": "https://maineballot.org/june%202018%20election/Q1_RankedChoiceVoting/",
+        "teaser":"https://maineballot.org/assets/img/2018_06/Question1_RankedChoice.jpg"},{
+        "title": "Question 1: Home Care",
+        "excerpt":"Results This measure did not pass. The gist This referendum would create a new Universal Home Care Program to provide in-home care to disabled people and seniors by taxing high-income earners. Ballot question “Do you want to create the Universal Home Care Program to provide home-based assistance to people with...","categories": ["November 2018 Election"],
+        "tags": ["Healthcare","Citizen Initiative","Did Not Pass"],
+        "url": "https://maineballot.org/november%202018%20election/Q1_HomeCare/",
+        "teaser":"https://maineballot.org/assets/img/2018_11/Question1_HomeCare.jpg"},{
+        "title": "Question 2: Wastewater",
+        "excerpt":"Results This measure passed. The gist Passing this question would allow the state to borrow $30 million to improve water quality. Ballot question “Do you favor a $30,000,000 bond issue to improve water quality, support the planning and construction of wastewater treatment facilities and assist homeowners whose homes are served...","categories": ["November 2018 Election"],
+        "tags": ["Water","Bond","Passed"],
+        "url": "https://maineballot.org/november%202018%20election/Q2_Wastewater/",
+        "teaser":"https://maineballot.org/assets/img/2018_11/Question2_Wastewater.jpg"},{
+        "title": "Question 3: Transportation",
+        "excerpt":"Results This measure passed. The gist Passing this question would allow the state to borrow $106 million for transportation improvements. This is the second of 3 consecutive years of transportation bonds that will fill a shortfall in the highway fund.1 Ballot question “Do you favor a $106,000,000 bond issue, including...","categories": ["November 2018 Election"],
+        "tags": ["Transportation","Bond","Passed"],
+        "url": "https://maineballot.org/november%202018%20election/Q3_Transportation/",
+        "teaser":"https://maineballot.org/assets/img/2018_11/Question3Transportation.jpg"},{
+        "title": "Question 4: University of Maine",
+        "excerpt":"Results This measure passed. The gist Approving this question would allow the state to borrow $49 million, matched by $49 million in private and public funds, to improve facilities and infrastructure at the University of Maine campuses. Ballot question “Do you favor a $49,000,000 bond issue to be matched by...","categories": ["November 2018 Election"],
+        "tags": ["Education","Bond","Passed"],
+        "url": "https://maineballot.org/november%202018%20election/Q4_University/",
+        "teaser":"https://maineballot.org/assets/img/2018_11/Question4_University.jpg"},{
+        "title": "Question 5: Community Colleges",
+        "excerpt":"Results This measure passed. The gist Approving this question would allow the state to borrow $15 million to upgrade facilities at all 7 of Maine’s community colleges. Ballot question “Do you favor a $15,000,000 bond issue to improve educational programs by upgrading facilities at all 7 of Maine’s community colleges...","categories": ["November 2018 Election"],
+        "tags": ["Education","Bond","Passed"],
+        "url": "https://maineballot.org/november%202018%20election/Q5_CommunityCollege/",
+        "teaser":"https://maineballot.org/assets/img/2018_11/Question5_CommunityCollege.jpg"},{
+        "title": "Question 1: Transportation",
+        "excerpt":"Results This measure passed. The gist Passing this question would allow the state to borrow $105 million, matched by $137 million in federal funds, for transportation improvements. This is the third of 3 consecutive years of transportation bonds that will fill a shortfall in the highway fund.1 Ballot question “Do...","categories": ["November 2019 Election"],
+        "tags": ["Transportation","Bond","Passed"],
+        "url": "https://maineballot.org/november%202019%20election/Q1_Transportation/",
+        "teaser":"https://maineballot.org/assets/img/2018_11/Question3Transportation.jpg"},{
+        "title": "Question 2: Alternative Signatures",
+        "excerpt":"Results This measure passed. The gist This resolution would allow people with physical disabilities that prevent them from physically signing an original signature to sign petitions using an alternative method, such as a signature stamp or having someone sign on their behalf. Ballot question “Do you favor amending the Constitution...","categories": ["November 2019 Election"],
+        "tags": ["Constitutional Resolution","Accessibility","Petitions","Passed"],
+        "url": "https://maineballot.org/november%202019%20election/Q2_AltSignature/",
+        "teaser":"https://maineballot.org/assets/img/2019_11/alt-signature.jpg"}]
