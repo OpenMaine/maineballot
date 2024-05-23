c9c55bd94d38322d4f9ac25550d02026f5c34ea8
JB Hutch <jbhutch01@gmail.com>
2024-05-23 12:50:01 -0400

    Install eslint

---
+++ astro/eslint.config.js
@@ -0,0 +1,33 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  astro: true,
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
+  ignores: [
+    // # build output
+    'dist/',
+
+    // # generated types
+    '.astro/',
+
+    // # dependencies
+    'node_modules/',
+
+    // # logs
+    'npm-debug.log*',
+    'yarn-debug.log*',
+    'yarn-error.log*',
+    'pnpm-debug.log*',
+
+    // # environment variables
+    '.env',
+    '.env.production',
+
+    // # macOS-specific files
+    '.DS_Store',
+
+    'src/assets/js/*',
+  ],
+})
