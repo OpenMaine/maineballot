80d8e1c1259b370fd43a0b8295d74efd5059c10c
JB Hutch <jbhutch01@gmail.com>
2024-06-24 11:22:45 -0400

    Use neutral instead of gray

--- src/components/react/mobile-menu.tsx
+++ src/components/react/mobile-menu.tsx
@@ -29,16 +29,16 @@ export function MobileMenu({
 
   return (
     <div>
-      <button {...api.getTriggerProps()} className="relative h-8 cursor-pointer bg-gray-500 px-1.5 text-white">
+      <button {...api.getTriggerProps()} className="relative h-8 cursor-pointer bg-neutral-500 px-1.5 text-white">
         <span className="sr-only">Toggle menu</span>
         <div {...api.getIndicatorProps()} className="relative h-1 w-6 bg-current transition-colors duration-300 before:absolute before:-top-2 before:left-0 before:h-1 before:w-6 before:origin-center before:bg-current before:transition-all before:duration-300 after:absolute after:left-0 after:top-2 after:h-1 after:w-6 after:origin-center after:bg-current after:transition-all after:duration-300 data-[state=open]:bg-transparent before:data-[state=open]:top-0 before:data-[state=open]:rotate-45 after:data-[state=open]:top-0 after:data-[state=open]:-rotate-45"></div>
       </button>
       <div {...api.getPositionerProps()}>
-        <div {...api.getContentProps()} className="hidden divide-y divide-gray-100 rounded-md border border-gray-100 bg-white p-1.5 shadow-lg data-[state=open]:block">
+        <div {...api.getContentProps()} className="hidden divide-y divide-neutral-100 rounded-md border border-neutral-100 bg-white p-1.5 shadow-lg data-[state=open]:block">
           {
             NAVIGATION.main.map(link => (
               <a
-                className="block px-5 py-2.5 text-gray-500"
+                className="block px-5 py-2.5 text-neutral-500"
                 aria-current={localActivePath === link.url ? 'page' : undefined}
                 key={link.url}
                 href={link.url}
