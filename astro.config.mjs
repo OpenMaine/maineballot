8487132b86521dcf055388de122c870e102eaf71
Jonathan Hutchison <jbhutch01@gmail.com>
2024-10-05 19:04:43 -0400

    Implement redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,7 +56,17 @@ export default defineConfig({
 
   output: 'static',
   adapter: netlify(),
-  // redirects: {
-  //   '/blog/old-post': '/blog/new-post',
-  // },
+  redirects: {
+    '/november%202023%20election/*': '/ballot-question/2023-11-07/:slug',
+    '/november%202021%20election/*': '/ballot-question/2021-11-02/:slug',
+    '/july%202020%20election/*': '/ballot-question/2020-07-14/:slug',
+    '/march%202020%20election/*': '/ballot-question/2020-03-03/:slug',
+    '/november%202019%20election/*': '/ballot-question/2019-11-05/:slug',
+    '/november%202018%20election/*': '/ballot-question/2018-11-06/:slug',
+    '/june%202018%20election/*': '/ballot-question/2018-06-12/:slug',
+    '/november%202017%20election/*': '/ballot-question/2017-11-07/:slug',
+    '/june%202017%20election/*': '/ballot-question/2017-06-13/:slug',
+    '/november%202016%20election/*': '/ballot-question/2016-11-08/:slug',
+    '/categories': '/elections',
+  },
 })
